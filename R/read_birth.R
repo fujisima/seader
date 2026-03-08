64f997dcdd74b162115e6454110d240c43c6542e
Leonardo Fujisima <leonardo@fujisimaanalytics.com>
2026-03-08 09:14:11 -0300

    Add birth by age dataset

--- R/read_birth.R
+++ R/read_birth.R
@@ -7,7 +7,7 @@
 #'
 #' @export
 
-read_birth <- function(
+read_birth_sex <- function(
     year = NULL,
     as_data_frame = FALSE,
     showProgress = TRUE,
@@ -22,8 +22,76 @@ read_birth <- function(
   cache_dir <- getOption("seader.cache_dir", path.expand("~/.seader"))
   if (!dir.exists(cache_dir)) dir.create(cache_dir, recursive = TRUE)
 
-  url <- "https://github.com/fujisima/seader-data/releases/download/v2025/nascidosvivos-sexo-2019-2025.parquet"
+  url <- "https://github.com/fujisima/seader-data/releases/download/birth-sex/nascidosvivos-sexo-2019-2025.parquet"
   dest <- file.path(cache_dir, "nascidosvivos-sexo-2019-2025.parquet")
+
+  # ---- Download logic ----
+  if (!isTRUE(cache) || !file.exists(dest)) {
+    if (file.exists(dest)) file.remove(dest)
+
+    ok <- tryCatch({
+      utils::download.file(
+        url = url,
+        destfile = dest,
+        mode = "wb",
+        quiet = !isTRUE(showProgress)
+      )
+      TRUE
+    }, error = function(e) FALSE)
+
+    if (!ok || !file.exists(dest)) {
+      stop("Failed to download births data.", call. = FALSE)
+    }
+  }
+
+  # ---- Open dataset ----
+  ds <- arrow::open_dataset(dest, format = "parquet")
+
+  # ---- Optional filtering by year ----
+  if (!is.null(year)) {
+
+    if (!is.numeric(year) || length(year) != 1) {
+      stop("`year` must be a single numeric value.", call. = FALSE)
+    }
+
+    year <- as.integer(year)
+    ds <- dplyr::filter(ds, ano == !!year)
+  }
+
+  # ---- Return ----
+  if (isTRUE(as_data_frame)) {
+    return(as.data.frame(dplyr::collect(ds)))
+  } else {
+    return(ds)
+  }
+}
+
+#' Read municipal live births by mother's age dataset
+#'
+#' @param year Integer year to filter (optional).
+#' @param as_data_frame Logical. Return data.frame if TRUE.
+#' @param showProgress Logical. Show download progress.
+#' @param cache Logical. Use cached file if TRUE.
+#'
+#' @export
+
+read_birth_mother_age <- function(
+    year = NULL,
+    as_data_frame = FALSE,
+    showProgress = TRUE,
+    cache = TRUE
+) {
+
+  if (!requireNamespace("arrow", quietly = TRUE)) {
+    stop("Package 'arrow' is required. Install it with install.packages('arrow').", call. = FALSE)
+  }
+
+  # ---- Cache directory ----
+  cache_dir <- getOption("seader.cache_dir", path.expand("~/.seader"))
+  if (!dir.exists(cache_dir)) dir.create(cache_dir, recursive = TRUE)
+
+  url <- "https://github.com/fujisima/seader-data/releases/download/birth-age/nascidosvivos-idade-2000-2010-2023-2024-2025.parquet"
+  dest <- file.path(cache_dir, "nascidosvivos-idade-2000-2010-2023-2024-2025.parquet")
 
   # ---- Download logic ----
   if (!isTRUE(cache) || !file.exists(dest)) {
